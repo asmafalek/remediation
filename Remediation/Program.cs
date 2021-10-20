56e5e0a9193014e7d1b6b06528049f28eb27123c
pootshy <asmamayssam@gmail.com>
2021-10-20 13:52:33 +0200

    singleton1

--- Remediation/Program.cs
+++ Remediation/Program.cs
@@ -14,6 +14,23 @@ namespace Remediation
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            
+            /////////////////////////////SINGELTON//////////////////////////////////////
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+
+
+            /////////////////////////////SINGELTON//////////////////////////////////////
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- Remediation/Singleton.cs
+++ Remediation/Singleton.cs
@@ -7,13 +7,40 @@ namespace Remediation
 {
     public class Singleton
     {
-        private static readonly Lazy<Singleton> lazy =
-        new Lazy<Singleton>(() => new Singleton());
+        private static Singleton instance;
+        private List<string> _servers = new List<string>();
+        private Random _random = new Random();
 
-        public static Singleton Instance => lazy.Value;
 
-        private Singleton() { }
 
-        public string CreateMessage() => "Message";
+        private Singleton() {
+            _servers.Add("ServerI");
+            _servers.Add("ServerII");
+            _servers.Add("ServerIII");
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        internal static Singleton GetInstance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Server
+        {
+            get
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r].ToString();
+            }
+        }
     }
 }
